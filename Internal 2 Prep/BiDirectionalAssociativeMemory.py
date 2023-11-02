0439c4fc145ceec6fe8b4727aa10b43ffe6d6801
TougherMocha93 <aseelama@gitam.in>
2023-11-02 13:13:23 +0530

    commit

--- Internal 2 Prep/BiDirectionalAssociativeMemory.py
+++ Internal 2 Prep/BiDirectionalAssociativeMemory.py
@@ -1,44 +1,18 @@
-import math #BACK PROP
+import numpy as np #BAM
 
-def sigmoid(y): return (1 / (1 + math.exp(y)))
-def partS(Y): return(Y * (1 - Y))
+p1, revP1 = np.array([-1, 1]), np.array([1, -1])
+p2, revP2 = np.array([1, 1]), np.array([-1, -1])
 
-alpha = 0.5
-x1, x2 = 0.2, 0.4
-w11, w12, w21, w22 = 1, 1, 1, 1
-v1, v2 = 1, 1
-b1, b2 = 1, 1
-t = 0.4
-error = 1
+x = [1, 1, 1, 1, -1, -1, 1, 1, 1, 1, -1, -1, 1, 1, 1]
+y = [1, -1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1]
 
-def forwardPass():
-    global error, Y, v1, v2, w11, w12, w21, w22, b1, b2, H1, H2
+e = np.array([p1 if bool_val == 1 else revP1 for bool_val in x])
+h = np.array([p2 if bool_val == 1 else revP2 for bool_val in y])
+w = e + h
+Y = np.dot(x, w)
+Y_rev = np.dot(p1, w.transpose())
 
-    h1 = x1 * w11 + x2 * w21
-    h2 = x1 * w12 + x2 * w22
-    H1, H2 = sigmoid(h1), sigmoid(h2)
-    y = H1 * v1 + H2 * v2
-    Y = sigmoid(y)
-    error = t - Y
+if Y.all() == p1.all() and Y_rev.all() == Y.all():
+  print("We can construct Bi-directional Associative Memory")
+else:  print("We cannot construct Bi-directional Associative Memory")
 
-def backwardPass():
-    global Y, v1, v2, w11, w12, w21, w22, b1, b2, H1, H2
-    Dy = error * partS(Y)
-    DH1 = v1 * Dy * partS(H1)
-    DH2 = v2 * Dy * partS(H2)
-    v1 += alpha * Dy * H1
-    v2 += alpha * Dy * H2
-    w11 += alpha * DH1 * x1
-    w12 += alpha * DH2 * x1
-    w21 += alpha * DH1 * x2
-    w22 += alpha * DH2 * x2
-    b1 += alpha * DH2
-    b2 += alpha * Dy
-
-count = 0
-while error > 0:
-    forwardPass()
-    backwardPass()
-    count += 1
-
-print(v1, v2, w11, w12, w21, w22, b1, b2, "\nerror =", error, "count =", count)
--- Internal 2 Prep/tendoolbp.py
+++ Internal 2 Prep/tendoolbp.py
@@ -1,54 +1,44 @@
-import math
+import math #BACK PROP
 
 def sigmoid(y): return (1 / (1 + math.exp(y)))
 def partS(Y): return(Y * (1 - Y))
 
 alpha = 0.5
-
-x1 = 0.2
-x2 = 0.4
-
+x1, x2 = 0.2, 0.4
 w11, w12, w21, w22 = 1, 1, 1, 1
 v1, v2 = 1, 1
-
 b1, b2 = 1, 1
-
 t = 0.4
 error = 1
 
 def forwardPass():
     global error, Y, v1, v2, w11, w12, w21, w22, b1, b2, H1, H2
-    
+
     h1 = x1 * w11 + x2 * w21
     h2 = x1 * w12 + x2 * w22
     H1, H2 = sigmoid(h1), sigmoid(h2)
-    
     y = H1 * v1 + H2 * v2
     Y = sigmoid(y)
-    
     error = t - Y
 
 def backwardPass():
     global Y, v1, v2, w11, w12, w21, w22, b1, b2, H1, H2
-    
     Dy = error * partS(Y)
-    
     DH1 = v1 * Dy * partS(H1)
     DH2 = v2 * Dy * partS(H2)
-    
     v1 += alpha * Dy * H1
     v2 += alpha * Dy * H2
-    
     w11 += alpha * DH1 * x1
     w12 += alpha * DH2 * x1
     w21 += alpha * DH1 * x2
     w22 += alpha * DH2 * x2
-    
     b1 += alpha * DH2
     b2 += alpha * Dy
 
-while error > 0.001:
+count = 0
+while error > 0:
     forwardPass()
     backwardPass()
+    count += 1
 
-print(v1, v2, w11, w12, w21, w22, b1, b2, "\nerror =", error)
+print(v1, v2, w11, w12, w21, w22, b1, b2, "\nerror =", error, "count =", count)
